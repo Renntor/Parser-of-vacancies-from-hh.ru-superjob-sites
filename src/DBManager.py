73c147d42e66f845b5dfdb70b77538c342fb363c
Denis <denis30510@gmail.com>
2023-07-17 16:22:12 +0300

    delete Saver. added utils.py

--- src/DBManager.py
+++ src/DBManager.py
@@ -4,20 +4,21 @@ import os
 
 class DBManager:
 
-    user = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    database = os.environ.get('DATABASE')
+    def __init__(self, database: str, user: str, password: str):
+        self.database = database
+        self.user = user
+        self.password = password
+        self.conn = psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password)
+        self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self) -> list:
         '''
         Получает список всех компаний и количество вакансий у каждой компании.
         :return:
         '''
-        with psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password) as conn:
-            with conn.cursor() as cur:
-                cur.execute("""SELECT employers.employer_name, COUNT(vacancy.*) FROM employers
-                                INNER JOIN vacancy USING(employer_id) GROUP BY employers.employer_name""")
-                rows = cur.fetchall()
+        self.cur.execute("""SELECT employers.employer_name, COUNT(vacancy.vacancy_id) AS count_vacancy FROM employers
+                       INNER JOIN vacancy USING(employer_id) GROUP BY employers.employer_name""")
+        rows = self.cur.fetchall()
         return rows
 
     def get_all_vacancies(self) -> list:
@@ -25,11 +26,9 @@ class DBManager:
         Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
         :return:
         '''
-        with psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password) as conn:
-            with conn.cursor() as cur:
-                cur.execute("""SELECT employers.employer_name, vacancy_name, salary_from, salary_to, url
-                                FROM vacancy INNER JOIN employers USING(employer_id)""")
-                rows = cur.fetchall()
+        self.cur.execute("""SELECT employers.employer_name, vacancy_name, salary_from, salary_to, url
+                        FROM vacancy INNER JOIN employers USING(employer_id)""")
+        rows = self.cur.fetchall()
         return rows
 
 
@@ -38,10 +37,8 @@ class DBManager:
         Получает среднюю зарплату по вакансиям.
         :return:
         """
-        with psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password) as conn:
-            with conn.cursor() as cur:
-                cur.execute('SELECT AVG(salary_from), AVG(salary_to) FROM vacancy')
-                rows = cur.fetchall()
+        self.cur.execute('SELECT AVG(salary_from), AVG(salary_to) FROM vacancy')
+        rows = self.cur.fetchall()
         return rows
 
     def get_vacancies_with_higher_salary(self) -> list:
@@ -49,20 +46,17 @@ class DBManager:
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         :return:
         '''
-        with psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password) as conn:
-            with conn.cursor() as cur:
-                cur.execute("SELECT * FROM vacancy\
-                 WHERE (SELECT AVG(salary_from+salary_to) FROM vacancy)< salary_from+salary_to")
-                rows = cur.fetchall()
-        return rows
+        self.cur.execute("SELECT * FROM vacancy\
+         WHERE (SELECT AVG(salary_from+salary_to) FROM vacancy)< salary_from+salary_to")
+        rows = self.cur.fetchall()
+        return
+
     def get_vacancies_with_keyword(self, name) -> list:
         '''
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”.
         :return:
         '''
-        with psycopg2.connect(host='localhost', database=self.database, user=self.user, password=self.password) as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM vacancy WHERE vacancy_name LIKE '%{name}%'")
-                rows = cur.fetchall()
+        self.cur.execute(f"SELECT * FROM vacancy WHERE vacancy_name LIKE '%{name}%'")
+        rows = self.cur.fetchall()
         return rows
 
--- src/main.py
+++ src/main.py
@@ -1,16 +1,49 @@
+from src.utils import employees_list_processing, vacancy_list_processing, adding_date_in_sql
 from src.Employers import Employers
-from src.Saver import Saver
-
-
-def main():
-    employers = Employers()
-    saver = Saver()
-
-    info_empl = employers.get_info_employers()
-    info_vac = employers.get_info_vacancies()
-    saver.save_employers(info_empl)
-    saver.save_vacancy(info_vac)
+from src.DBManager import DBManager
+import psycopg2
 
 
 if __name__ == '__main__':
-    main()
+    employers = Employers()
+    employees_list = employees_list_processing(employers.get_info_employers())
+    vacancy_list = vacancy_list_processing(employers.get_info_vacancies())
+
+    user = input('Привет! Напиши данные для входа в базу данных \nИмя пользователя: ')
+    password = input('Пароль: ')
+    database = input('Название базы данных: ')
+    try:
+        dbmanager = DBManager(database, user, password)
+        adding_date_in_sql(database, user, password, employees_list, vacancy_list)
+    except psycopg2.OperationalError as e:
+        exit(f'{e}Неверные данные!')
+
+    while True:
+        user_input = input('Напиши номер данных, которые необходимо вывести?\n\
+Все компании и количество вакансий - 1\n\
+Все вакансии с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию - 2\n\
+Среднюю зарплату по вакансиям - 3\n\
+Все вакансии, у которых зарплата выше средней по всем вакансиям - 4\n\
+Найти вакансии с определенным именем - 5\n\
+Закончить работу - Стоп\n')
+
+        if user_input == '1':
+            for row in dbmanager.get_companies_and_vacancies_count():
+                print(row)
+        elif user_input == '2':
+            for row in dbmanager.get_all_vacancies():
+                print(row)
+        elif user_input == '3':
+            for row in dbmanager.get_avg_salary():
+                print(f'от {round(row[0])} до {round(row[1])}')
+        elif user_input == '4':
+            for row in dbmanager.get_vacancies_with_higher_salary():
+                print(row)
+        elif user_input == '5':
+            job_name = input('Название вакансии: ')
+            for row in dbmanager.get_vacancies_with_keyword(job_name):
+                print(row)
+        elif user_input.lower() == 'стоп':
+            break
+        else:
+            print('Неверные данные!')
